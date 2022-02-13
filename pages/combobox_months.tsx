ef07ee2e707b4140f75f3e8914a96e490f1aaf02
latteS <90080815+LatteSyobon@users.noreply.github.com>
2022-02-13 21:51:00 +0900

    Add combo box.

---
+++ pages/combobox_months.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '../styles/Home.module.css'
+
+function ComboBoxMonths() {
+    return (
+        <select name="month">
+            <option value="">-</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            </select>
+    );
+}
+
+export default ComboBoxMonths;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import ComboBoxYears from './combobox_years'
+import ComboBoxDays from './combobox_days'
+import ComboBoxMonths from './combobox_months'
 
 const Home: NextPage = () => {
   return (
@@ -22,6 +24,10 @@ const Home: NextPage = () => {
         You can tell how old you are now by the year you were born! - 生まれた年代で自分の今の年齢がわかるよ！
         </p>
         <p>year of birth - 生まれた年</p><ComboBoxYears></ComboBoxYears>
+        <p>month of birth - 生まれた月</p><ComboBoxMonths></ComboBoxMonths>
+        <p>day of birth - 生まれた日</p><ComboBoxDays></ComboBoxDays>
+        <p>Press this button here, and it'll tell you your age. - ここのボタンを押すと年齢がわかるよ</p>
+        <button>Verification! - 確認!</button>
         <div className={styles.grid}>
         </div>
       </main>
